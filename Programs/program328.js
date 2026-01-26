ee79aea572f98b7f74e783b0c13bdfeef71b216d
Varsha Tavdival <varshatavdival@gmail.com>
2026-01-26 21:49:57 +0530

    Add files via upload

---
+++ Programs/program328.js
@@ -0,0 +1,13 @@
+
+let data = "Marvellous";
+
+console.log(data);
+console.log(typeof(data));
+
+let str = 'A';
+
+console.log(str);
+console.log(typeof(str));
+
+
+
---
+++ Programs/program329.js
@@ -0,0 +1,8 @@
+
+let data = true;
+
+console.log(data);
+console.log(typeof(data));
+
+
+
